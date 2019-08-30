a4a41b9db263997215809fdeb665fc2320793b7d
uf0 <scnepa222@gmail.com>
2019-08-30 14:42:26 +0200

    ghpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import AppContainer from "./components/AppContainer";
 import Home from "./pages/Home";
 import Path from "./pages/Path";
